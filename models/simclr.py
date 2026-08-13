2135928327ef72ab40d99134e6c25a9b061614ca
Aleena Saleem <161963020+aleena-saleem@users.noreply.github.com>
2026-08-13 19:07:24 +0500

    Update simclr.py

--- models/simclr.py
+++ models/simclr.py
@@ -1,8 +1,8 @@
-
 import torch
 import torch.nn as nn
 
 from models.resnet_encoder import ResNetEncoder
+from models.vit_encoder import ViTEncoder
 from models.projection_head import ProjectionHead
 
 
@@ -10,30 +10,45 @@ class SimCLR(nn.Module):
 
     def __init__(
         self,
+        encoder_type="resnet",
         pretrained_encoder=True,
         projection_dim=128
     ):
 
         super().__init__()
 
-        # Encoder
-        self.encoder = ResNetEncoder(
-            pretrained=pretrained_encoder
-        )
+        if encoder_type == "resnet":
 
-        # Projection head
+            self.encoder = ResNetEncoder(
+                pretrained=pretrained_encoder
+            )
+
+            input_dim = 2048
+
+        elif encoder_type == "vit":
+
+            self.encoder = ViTEncoder(
+                pretrained=pretrained_encoder
+            )
+
+            input_dim = self.encoder.feature_dim
+
+        else:
+
+            raise ValueError(
+                "encoder_type must be 'resnet' or 'vit'"
+            )
+
         self.projector = ProjectionHead(
-            input_dim=2048,
+            input_dim=input_dim,
             hidden_dim=512,
             output_dim=projection_dim
         )
 
     def forward(self, x):
 
-        # Representation from encoder
         h = self.encoder(x)
 
-        # Projection for contrastive learning
         z = self.projector(h)
 
         return h, z
